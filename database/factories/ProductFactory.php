252e8188ace6b6b3f9559ee7166de93188b7e026
cuong <vancuong9362@gmail.com>
2026-09-15 16:35:37 +0700

    seeder-factory-filler

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Model;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Model>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+        'category_id' => fake()->numberBetween(1, 5),
+        'name' => fake()->unique()->words(3, true),
+        'decription' => fake()->paragraph(), 
+        'price' => fake()->randomFloat(2, 100, 5000),
+        'stock' => fake()->numberBetween(10, 100),
+        ];
+    }
+}
